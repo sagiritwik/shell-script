92a6d0961d0c13b6964fcc45846a47f756d4b0fb
sagiritwik <ritwikrao7@gmail.com>
2024-05-10 10:37:57 +0530

    added 2nd script

--- 08-install-packages.sh
+++ 08-install-packages.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+ID=$(id -u)
+
+ if [ $ID -ne 0 ]
+ then
+        echo -e "$R ERROR:: Please run the script with the root access $N"
+        exit 1 # other than 0
+ else
+        echo "you're root user"
+ fi     
+
+ echo "All arguments passed: $@"
